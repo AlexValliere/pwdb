0ddbf89dbcef1228ca2c8184d8856ab7a7114a79
Alexandre Petit <alexandre.petit.vilapon@gmail.com>
2016-04-06 01:30:21 +0200

    moved footer in the new layout

--- view/layout_post.php
+++ view/layout_post.php
@@ -1,8 +1,7 @@
+					<?php include("view/layout/footer.php"); ?>
 				</div>
 			</div>
 		</div>
-
-		<?php include("view/layout/footer.php"); ?>
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
