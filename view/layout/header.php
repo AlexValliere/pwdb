3809ef029cb702bd838ddce5a59f91ee52dcfe47
Alexandre Petit <alexandre.petit.vilapon@gmail.com>
2016-04-06 01:21:03 +0200

    Added bootstrap

--- view/layout/header.php
+++ view/layout/header.php
@@ -1,15 +1,27 @@
-<header>
-	<a href="index.php"><p>Panzer Waltz DataBase</p></a>
-	<nav id="menu">
-		<ul>
-			<li><a href="index.php?route=home">Home</a></li>
-			<?php if ($current_user == NULL) { ?>
-				<li><a href="index.php?route=login">Connexion</a></li>
-				<li><a href="index.php?route=register">Inscription</a></li>
-			<?php } else { ?>
-				<li><a href="index.php?route=create_metal_maiden">Add new Metal Maiden</a></li>
-				<li><a href="index.php?route=logout">Déconnexion</a></li>
-			<?php } ?>
-		</ul>
-	</nav>
-</header>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php?route=home">Panzer Waltz DataBase</a>
+		</div>
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<?php if ($current_user == NULL) { ?>
+					<li><a href="index.php?route=login">Connexion</a></li>
+					<li><a href="index.php?route=register">Inscription</a></li>
+				<?php } else { ?>
+					<li><a href="index.php?route=create_metal_maiden">Add new Metal Maiden</a></li>
+					<li><a href="index.php?route=logout">Déconnexion</a></li>
+				<?php } ?>
+			</ul>
+			<form class="navbar-form navbar-right">
+				<input type="text" class="form-control" placeholder="Search...">
+			</form>
+		</div>
+	</div>
+</nav>
--- view/layout_pre.php
+++ view/layout_pre.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
-		<link href='https://fonts.googleapis.com/css?family=Roboto:300italic,300' rel='stylesheet' type='text/css'>
 		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
 		<title>Panzer Waltz DB</title>
+
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+		<link href="css/dashboard.css" rel="stylesheet">
+
 		<link rel="stylesheet" href="view/layout/layout.css">
 		<?php if (isset($routes[$route]) && get_route_css($routes[$route]) != NULL) echo '<link rel="stylesheet" href="'.get_route_css($routes[$route]).'">'; ?>
-		<!--<script src="script.js"></script>-->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
-			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 	</head>
 	<body>
